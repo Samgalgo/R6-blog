a546ec4ad148aa91775899fe4fb8ee4d32279ff5
Samantha Galindo <bluesundaysg@gmail.com>
2022-05-16 22:09:11 -0400

    Second part of the associations lesson is finally done :)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_01_232224) do
+ActiveRecord::Schema.define(version: 2022_05_15_023455) do
+
+  create_table "comentarios", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comentarios_on_post_id"
+  end
+
+  create_table "coments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_coments_on_post_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "gatos", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_gatos_on_post_id"
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
@@ -27,4 +61,6 @@ ActiveRecord::Schema.define(version: 2022_05_01_232224) do
     t.text "body"
   end
 
+  add_foreign_key "comentarios", "posts"
+  add_foreign_key "gatos", "posts"
 end
